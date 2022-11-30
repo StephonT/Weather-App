fe444107ff1c260b184be7aa99838d5191a562ff
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-11-30 16:37:30 -0500

    conditional rendering

--- showmetheweather/pages/index.js
+++ showmetheweather/pages/index.js
@@ -43,6 +43,7 @@ export default function Home() {
           src="https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2xvdWR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
           layout="fill"
           className="object-cover"
+          alt='/'
         />
 
         {/* Search */}
