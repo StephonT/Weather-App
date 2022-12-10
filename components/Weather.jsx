6469d00279dfe4e24c8fc37556a45ede942c740f
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-10 18:54:09 -0500

    fixed styling

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import React from "react";
 
 const Weather = ({ data }) => {
   console.log(data);
 
   return (
-    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
+    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[80vh] m-auto p-4 text-gray-300 z-10">
       {/* Top */}
       <div className="relative flex justify-between pt-12">
         <div className="flex flex-col items-center">
